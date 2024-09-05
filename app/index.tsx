1e333a5c66561aedc1a6c9e2fb8586c8c8adaf56
Nazar Klymenko <nazar.klymenko187@gmail.com>
2024-09-05 17:55:02 +0200

    ui: full page loading when redirecting to initial route

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,12 @@
-import { Redirect } from "expo-router";
+import { FullPageLoading } from "@/components/layout/FullPageLoading";
+import { PageContainer } from "@/components/layout/PageContainer";
+import { Redirect, Stack } from "expo-router";
 
 export default function Index() {
-  return <Redirect href="/events" />;
+  return (
+    <FullPageLoading>
+      <Stack.Screen options={{ headerShown: false }} />
+      <Redirect href="/events" />
+    </FullPageLoading>
+  );
 }
--- components/layout/FullPageLoading.tsx
+++ components/layout/FullPageLoading.tsx
@@ -1,17 +1,9 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
+import { Spinner, Stack, StackProps, YStack, useTheme } from "tamagui";
 
-import { ScrollView, ViewStyle } from "react-native";
-
-import {
-  Spinner,
-  Stack,
-  StackProps,
-  YStack,
-  useMedia,
-  useTheme,
-} from "tamagui";
-
-export function FullPageLoading() {
+export const FullPageLoading: React.FC<PropsWithChildren<{}>> = ({
+  children,
+}) => {
   const theme = useTheme();
 
   const containerStyle: StackProps = {
@@ -22,7 +14,7 @@ export function FullPageLoading() {
     marginHorizontal: "auto",
   };
 
-  const content = (
+  return (
     <YStack flex={1} backgroundColor="$background">
       <YStack
         {...containerStyle}
@@ -33,9 +25,8 @@ export function FullPageLoading() {
         alignItems="center"
       >
         <Spinner color="$accentColor" size="large" />
+        {children}
       </YStack>
     </YStack>
   );
-
-  return content;
-}
+};
