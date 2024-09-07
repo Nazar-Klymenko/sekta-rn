59c5d76ed93059e2ed8bf3ca762544b739e90504
Nazar Klymenko <nazar.klymenko187@gmail.com>
2024-09-08 00:33:17 +0200

    Update firebase.ts

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,18 +1,15 @@
 // firebaseConfig.ts
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { FirebaseApp, getApps, initializeApp } from "firebase/app";
-import {
-  Auth,
-  getAuth,
-  getReactNativePersistence,
-  initializeAuth,
-} from "firebase/auth";
-import { Firestore, getFirestore } from "firebase/firestore";
+import { Auth, getAuth, initializeAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-import { Platform } from "react-native";
+import {
+  getAuthToken,
+  removeAuthToken,
+  setAuthToken,
+} from "@/utils/tokenStorage";
 
-// Your Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
@@ -26,26 +23,16 @@ const firebaseConfig = {
 let app: FirebaseApp;
 let auth: Auth;
 
-// Initialize Firebase if not already initialized
 if (getApps().length === 0) {
   app = initializeApp(firebaseConfig);
-
-  // Configure Auth based on platform
-  if (Platform.OS === "web") {
-    auth = getAuth(app);
-  } else {
-    auth = initializeAuth(app, {
-      persistence: getReactNativePersistence(AsyncStorage),
-    });
-  }
+  auth = initializeAuth(app);
 } else {
   app = getApps()[0];
   auth = getAuth(app);
 }
 
-// const firestore: Firestore = getFirestore(app);
-const storage = getStorage(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
 export default app;
 export { auth, db, storage };
