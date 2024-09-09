38816034a6d9506152b9cb2d98bfc7de130afc32
Nazar Klymenko <nazar.klymenko187@gmail.com>
2024-09-09 13:55:54 +0200

    feat: package name

--- utils/tokenStorage.ts
+++ utils/tokenStorage.ts
@@ -4,20 +4,25 @@ import * as SecureStore from "expo-secure-store";
 
 const AUTH_TOKEN_KEY = "firebaseAuthToken";
 
+async function isSecureStoreAvailable() {
+  return await SecureStore.isAvailableAsync();
+}
+
 export const setAuthToken = async (token: string): Promise<void> => {
-  if (Platform.OS !== "web") {
+  if (Platform.OS !== "web" && (await isSecureStoreAvailable())) {
     await SecureStore.setItemAsync(AUTH_TOKEN_KEY, token);
   }
 };
 
 export const getAuthToken = async (): Promise<string | null> => {
-  if (Platform.OS !== "web") {
+  if (Platform.OS !== "web" && (await isSecureStoreAvailable())) {
     return await SecureStore.getItemAsync(AUTH_TOKEN_KEY);
-  } else return null;
+  }
+  return null;
 };
 
 export const removeAuthToken = async (): Promise<void> => {
-  if (Platform.OS !== "web") {
+  if (Platform.OS !== "web" && (await isSecureStoreAvailable())) {
     await SecureStore.deleteItemAsync(AUTH_TOKEN_KEY);
   }
 };
