7daf27ba6acd5b51e83ed4efc2651d3253ed3479
Yehor Kosiak <yegorkosyak32@gmail.com>
2024-09-23 14:57:52 +0200

    refactor: change font inter to spartan league

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,19 +1,43 @@
 import { config } from "@tamagui/config/v3";
-import { createInterFont } from "@tamagui/font-inter";
 
-import { createTamagui } from "tamagui";
+import { createFont, createTamagui } from "tamagui";
 
 import * as themes from "./theme/theme-output";
 
-const interFont = createInterFont();
+const leagueSpartanFace = {
+  100: { normal: "LeagueSpartan_100Thin" },
+  200: { normal: "LeagueSpartan_200ExtraLight" },
+  300: { normal: "LeagueSpartan_300Light" },
+  400: { normal: "LeagueSpartan_400Regular" },
+  500: { normal: "LeagueSpartan_500Medium" },
+  600: { normal: "LeagueSpartan_600SemiBold" },
+  700: { normal: "LeagueSpartan_700Bold" },
+  800: { normal: "LeagueSpartan_800ExtraBold" },
+  900: { normal: "LeagueSpartan_900Black" },
+};
+
+const headingFont = createFont({
+  size: config.fonts.heading.size,
+  lineHeight: config.fonts.heading.lineHeight,
+  weight: config.fonts.heading.weight,
+  letterSpacing: config.fonts.heading.letterSpacing,
+  face: leagueSpartanFace,
+});
+
+const bodyFont = createFont({
+  size: config.fonts.body.size,
+  lineHeight: config.fonts.body.lineHeight,
+  weight: config.fonts.body.weight,
+  letterSpacing: config.fonts.body.letterSpacing,
+  face: leagueSpartanFace,
+});
 
 export const tamaguiConfig = createTamagui({
   ...config,
   themes,
   fonts: {
-    ...config.fonts,
-    heading: interFont,
-    body: interFont,
+    heading: headingFont,
+    body: bodyFont,
   },
 });
 
