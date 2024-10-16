e0d4a1f1100aa519ff16bf1b14ae8989f63ed692
Yehor Kosiak <yegorkosyak32@gmail.com>
2024-10-17 00:33:15 +0200

    refactor: profile layout

--- app/(tabs)/profile/_layout.tsx
+++ app/(tabs)/profile/_layout.tsx
@@ -3,8 +3,6 @@ import React from "react";
 import { Platform } from "react-native";
 import { Text, TouchableOpacity } from "react-native";
 
-import { DrawerButton } from "@/features/core/components/drawer/DrawerButton";
-
 import { useTheme } from "tamagui";
 
 import { Slot, Stack } from "expo-router";
@@ -15,101 +13,67 @@ export default function HomeLayout() {
     return <Slot />;
   }
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        animation: "fade_from_bottom",
+        headerStyle: {
+          backgroundColor: theme.background.get(),
+        },
+        headerTintColor: theme.color.get(),
+        headerTitleAlign: "center",
+        headerTitleStyle: {
+          fontFamily: "LeagueSpartan_700Bold",
+          fontSize: 25,
+          color: "orange",
+        },
+      }}
+    >
       <Stack.Screen
         name="index"
         options={({ navigation }) => ({
           title: "Profile",
           animation: "fade_from_bottom",
-          headerShown: true || Platform.OS !== "web",
           headerRight: () => (
             <TouchableOpacity onPress={() => navigation.push("admin")}>
               <Text style={{ color: "orange", marginRight: 10 }}>Admin</Text>
             </TouchableOpacity>
           ),
-
-          headerStyle: {
-            backgroundColor: theme.background.get(),
-          },
-          headerTintColor: theme.color.get(),
-          headerTitleAlign: "center",
-          headerTitleStyle: {
-            fontFamily: "LeagueSpartan_700Bold",
-            fontSize: 25,
-            color: "orange",
-          },
         })}
       />
       <Stack.Screen
         name="change-email"
         options={{
           title: "Profile information",
-          animation: "fade_from_bottom",
-          headerShown: true || Platform.OS !== "web",
-          headerStyle: {
-            backgroundColor: theme.background.get(),
-          },
-          headerTintColor: theme.color.get(),
         }}
       />
       <Stack.Screen
         name="change-username"
         options={{
           title: "Change username",
-          animation: "fade_from_bottom",
-          headerShown: true || Platform.OS !== "web",
-          headerStyle: {
-            backgroundColor: theme.background.get(),
-          },
-          headerTintColor: theme.color.get(),
         }}
       />
       <Stack.Screen
         name="change-password"
         options={{
           title: "Change password",
-          animation: "fade_from_bottom",
-          headerShown: true || Platform.OS !== "web",
-          headerStyle: {
-            backgroundColor: theme.background.get(),
-          },
-          headerTintColor: theme.color.get(),
         }}
       />
       <Stack.Screen
         name="delete-profile"
         options={{
           title: "Delete account",
-          animation: "fade_from_bottom",
-          headerShown: true || Platform.OS !== "web",
-          headerStyle: {
-            backgroundColor: theme.background.get(),
-          },
-          headerTintColor: theme.color.get(),
         }}
       />
       <Stack.Screen
         name="push-notifications"
         options={{
           title: "Push notifications",
-          animation: "fade_from_bottom",
-          headerShown: true || Platform.OS !== "web",
-          headerStyle: {
-            backgroundColor: theme.background.get(),
-          },
-          headerTintColor: theme.color.get(),
         }}
       />
       <Stack.Screen
         name="email-notifications"
         options={{
           title: "Email notifications",
-          animation: "fade_from_bottom",
-          headerShown: true || Platform.OS !== "web",
-          headerStyle: {
-            backgroundColor: theme.background.get(),
-          },
-          headerTintColor: theme.color.get(),
         }}
       />
     </Stack>
