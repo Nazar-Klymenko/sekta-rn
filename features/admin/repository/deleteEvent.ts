4568815f8f0a60aa6501cb3fd6bf7e687f1190d9
Nazar Klymenko <nazar.klymenko187@gmail.com>
2024-11-13 23:23:10 +0100

    fix: delete event even if the image doesn't exist
    FIXME: Needs revision, not sure if it's the correct way.

--- features/admin/repository/deleteEvent.ts
+++ features/admin/repository/deleteEvent.ts
@@ -1,21 +1,31 @@
+import { FirebaseError } from "firebase/app";
 import { deleteDoc, doc } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
 
-import { Event } from "@/features/event/models/Event";
 import { db, storage } from "@/lib/firebase/firebase";
 
 interface DeleteEventParams {
   eventId: string;
   imageId: string;
 }
+
 export const deleteEvent = async ({ eventId, imageId }: DeleteEventParams) => {
+  const imageRef = ref(storage, `events/${imageId}`);
+
   try {
-    const imageRef = ref(storage, `events/${imageId}`);
-
     await deleteObject(imageRef);
+  } catch (error) {
+    if ((error as FirebaseError).code === "storage/object-not-found") {
+      console.warn(
+        `Image with ID ${imageId} does not exist. Proceeding to delete event.`
+      );
+    } else {
+      console.error("Error deleting image: ", error);
+    }
+  }
 
+  try {
     await deleteDoc(doc(db, "events", eventId));
-
     return { success: true };
   } catch (error) {
     console.error("Error deleting event: ", error);
