b66a6b920d02d95559efc3cf4e89707cc77591fd
Nazar Klymenko <nazar.klymenko187@gmail.com>
2024-11-17 15:27:25 +0100

    fix: remove gradient, should fix the shadow issue.
    i believe the shadow issue appears on emulators because of some GPU acceleration settings etc. the safest bet is to remove linear gradient. tested on a physical device.
    TODO: spacing overhaul, i left it without any spacing.

--- features/profile/screens/ProfileScreen/VerifyEmail.tsx
+++ features/profile/screens/ProfileScreen/VerifyEmail.tsx
@@ -8,7 +8,7 @@ import { useVerifyEmail } from "@/features/profile/hooks/useVerifyEmail";
 
 import { Mail } from "@tamagui/lucide-icons";
 
-import { Paragraph, Theme, XStack, YStack, styled } from "tamagui";
+import { Paragraph, SizableText, Theme, XStack, YStack, styled } from "tamagui";
 
 export const VerifyEmail = () => {
   const { user } = useAuth();
@@ -19,45 +19,35 @@ export const VerifyEmail = () => {
   }
   return (
     <Theme name="surface1">
-      <YStack marginVertical="$2">
-        <LinearGradient
-          colors={["$accentBackground", "$blue8Light"]}
-          start={[0, 0]}
-          end={[1, 1]}
-          borderRadius="$4"
-          padding={2}
-        >
-          <YStack
-            backgroundColor="$background"
-            padding="$4"
-            borderRadius="$3"
-            alignItems="flex-start"
-            blockSize="border-box"
-            gap="$3"
-          >
-            <YStack alignItems="flex-start" gap="$3">
-              <XStack alignItems="center" gap="$3">
-                <Mail size="$1" color="$color" />
-                <Paragraph size="$6" fontWeight={500}>
-                  Verify Your Email
-                </Paragraph>
-              </XStack>
-              <Paragraph fontSize="$5">
-                Please verify your email address to access all features.
-              </Paragraph>
-            </YStack>
+      <YStack
+        backgroundColor="$background"
+        padding="$4"
+        borderRadius="$4"
+        alignItems="flex-start"
+        blockSize="border-box"
+        gap="$4"
+      >
+        <YStack alignItems="flex-start" gap="$3">
+          <XStack alignItems="center" gap="$3">
+            <Mail size="$1" color="$color" />
+            <SizableText size="$6" fontWeight={500}>
+              Verify Your Email
+            </SizableText>
+          </XStack>
+          <SizableText size="$5">
+            Please verify your email address to access all features.
+          </SizableText>
+        </YStack>
 
-            <ButtonCTA
-              theme={"accent"}
-              color="white"
-              onPress={() => sendVerification()}
-              isLoading={isPending}
-              disabled={isPending}
-            >
-              Send Verification Email
-            </ButtonCTA>
-          </YStack>
-        </LinearGradient>
+        <ButtonCTA
+          theme={"accent"}
+          color="white"
+          onPress={() => sendVerification()}
+          isLoading={isPending}
+          disabled={isPending}
+        >
+          Send Verification Email
+        </ButtonCTA>
       </YStack>
     </Theme>
   );
