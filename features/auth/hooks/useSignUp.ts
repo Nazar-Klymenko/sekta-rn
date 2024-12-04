78fdedc4e1dff4128148b16c3be735cfb5941c3a
Yehor Kosiak <yegorkosyak32@gmail.com>
2024-12-04 22:24:43 +0100

    refactor: remove redundunt query invalidation

--- features/auth/hooks/useSignUp.ts
+++ features/auth/hooks/useSignUp.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 
 import { useOperationStatusHelper } from "@/features/core/hooks/useOperationStatusHelper";
 
@@ -6,8 +6,8 @@ import { signUp } from "../repository/signUp";
 import { SignUpSchemaType } from "../utils/schemas";
 
 export const useSignUp = () => {
-  const queryClient = useQueryClient();
   const handleToastMessage = useOperationStatusHelper();
+
   return useMutation({
     mutationFn: async ({ email, password, ...userData }: SignUpSchemaType) => {
       await signUp({
@@ -18,10 +18,11 @@ export const useSignUp = () => {
         agreeEmail: userData.agreeEmail,
       });
     },
+
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["user"] });
       handleToastMessage(null, "signup", "success");
     },
+
     onError: (error) => {
       handleToastMessage(error, "signup", "error");
     },
--- features/users/hooks/useDeleteProfile.ts
+++ features/users/hooks/useDeleteProfile.ts
@@ -1,15 +1,13 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 
 import { useOperationStatusHelper } from "@/features/core/hooks/useOperationStatusHelper";
 import { deleteProfile } from "@/features/users/api/deleteProfile";
 
 export const useDeleteProfile = () => {
-  const queryClient = useQueryClient();
   const handleToastMessage = useOperationStatusHelper();
   return useMutation({
     mutationFn: (password: string) => deleteProfile(password),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["user"] });
       handleToastMessage(null, "deleteAccount", "info");
     },
     onError(error) {
