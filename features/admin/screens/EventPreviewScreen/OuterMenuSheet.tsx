266063b523aa8f5a3eb6c4e42d1845d682759d9d
Nazar Klymenko <nazar.klymenko187@gmail.com>
2024-12-19 17:32:37 +0100

    style: sheet background color

--- features/admin/screens/EventPreviewScreen/OuterMenuSheet.tsx
+++ features/admin/screens/EventPreviewScreen/OuterMenuSheet.tsx
@@ -31,54 +31,52 @@ export function OuterMenuSheet({
   const router = useRouter();
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
-      <Theme name={"surface1"}>
-        <TStack flex={1} width="100%">
-          <Button
-            size={"$6"}
-            onPress={() => {
-              onOpenChange(false);
-              router.push({
-                pathname: "/admin/events/[id]/update",
-                params: { id: id },
-              });
-            }}
-            flex={1}
-            width="100%"
-            backgroundColor={"$"}
-            icon={Edit3}
-            borderBottomRightRadius={0}
-            borderBottomLeftRadius={0}
-            animation="quickest"
-            pressStyle={{
-              borderWidth: 0,
-              backgroundColor: "$background",
-              opacity: 0.7,
-            }}
-          >
-            Edit
-          </Button>
-          <Separator />
-          <Button
-            size={"$6"}
-            onPress={() => {
-              setInnerShowConfirmSheet(true);
-            }}
-            borderTopRightRadius={0}
-            borderTopLeftRadius={0}
-            width="100%"
-            icon={X}
-            color={"$red10Dark"}
-            animation="lazy"
-            pressStyle={{
-              borderWidth: 0,
-              backgroundColor: "$background",
-              opacity: 0.7,
-            }}
-          >
-            Delete
-          </Button>
-        </TStack>
-      </Theme>
+      <TStack flex={1} width="100%">
+        <Button
+          size={"$6"}
+          onPress={() => {
+            onOpenChange(false);
+            router.push({
+              pathname: "/admin/events/[id]/update",
+              params: { id: id },
+            });
+          }}
+          flex={1}
+          width="100%"
+          backgroundColor={"$"}
+          icon={Edit3}
+          borderBottomRightRadius={0}
+          borderBottomLeftRadius={0}
+          animation="quickest"
+          pressStyle={{
+            borderWidth: 0,
+            backgroundColor: "$background",
+            opacity: 0.7,
+          }}
+        >
+          Edit
+        </Button>
+        <Separator />
+        <Button
+          size={"$6"}
+          onPress={() => {
+            setInnerShowConfirmSheet(true);
+          }}
+          borderTopRightRadius={0}
+          borderTopLeftRadius={0}
+          width="100%"
+          icon={X}
+          color={"$red10Dark"}
+          animation="lazy"
+          pressStyle={{
+            borderWidth: 0,
+            backgroundColor: "$background",
+            opacity: 0.7,
+          }}
+        >
+          Delete
+        </Button>
+      </TStack>
     </Sheet>
   );
 }
