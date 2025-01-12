297b2ec1c210702782fac0dbcd6e5199ae9cd851
Yehor Kosiak <yegorkosyak32@gmail.com>
2025-01-12 15:48:10 +0100

    style: client request remove profile rotation

--- features/users/screens/ProfileScreen/ProfileHeader.tsx
+++ features/users/screens/ProfileScreen/ProfileHeader.tsx
@@ -1,9 +1,10 @@
+import fallbackImage from "@/assets/images/logo-big.png";
+
 import React from "react";
 
-import fallbackImage from "@/assets/images/logo-big.png";
-import { useAuth } from "@/features/auth/hooks/useAuth";
-
 import { Avatar, SizableText, XStack, YStack } from "tamagui";
+
+import { useAuth } from "@/features/auth/hooks/useAuth";
 
 export function ProfileHeader() {
   const { displayUser, isLoading } = useAuth();
@@ -23,7 +24,7 @@ export function ProfileHeader() {
         <SizableText fontSize="$6" fontWeight={700}>
           {displayUser.username}
         </SizableText>
-        <SizableText fontSize="$4" color="$gray10Light">
+        <SizableText fontSize="$4" color="gray">
           {displayUser.auth.email}
         </SizableText>
       </YStack>
@@ -34,7 +35,7 @@ export function ProfileHeader() {
         <SizableText fontSize="$6" fontWeight={700}>
           Guest
         </SizableText>
-        <SizableText fontSize="$4" color="$gray10Light">
+        <SizableText fontSize="$4" color="gray">
           Not logged in
         </SizableText>
       </YStack>
@@ -43,11 +44,7 @@ export function ProfileHeader() {
 
   return (
     <XStack alignItems="center" marginBottom={18}>
-      <Avatar
-        circular
-        size="$8"
-        style={{ transform: [{ rotate: "-30deg" }], marginRight: 16 }}
-      >
+      <Avatar circular size="$8" style={{ marginRight: 16 }}>
         <Avatar.Image src={fallbackImage} />
       </Avatar>
       {content}
